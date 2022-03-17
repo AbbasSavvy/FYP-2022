11f77641bb110c81bf7d91f30231c4a05afed9d1
vstark <shahv65@gmail.com>
2022-03-17 20:35:06 +0530

    Completed view compatibility
    Completed comparison of single JD with multiple resumes - backend

--- fyp_project/home/views.py
+++ fyp_project/home/views.py
@@ -68,35 +68,20 @@ def upload_file_view(request):
 def student(request):
     job_roles = Jd.objects.all()
     if request.method == 'POST':
-        # context = {
-        #     'skills' : request.POST.get('skills'),
-        #     'job_role' : request.POST.get('jobRole')
-        # }
+
         skills = request.POST.get('skills')
         job_role = request.POST.get('jobRole')
         jd = Jd.objects.filter(pk=job_role).first()
         jd = jd.get_job_desc()
 
-        filedocument = []
-        sentences = sent_tokenize(jd)
-        for sentence in sentences:
-            filedocument.append(sentence)
+        filedocument = sentence_tokenize(jd)
 
         gen_docs = [[w.lower() for w in word_tokenize(text)]
                     for text in filedocument]
         sw = set(stopwords.words('english'))
         gen_docs = [[w for w in list if w not in sw] for list in gen_docs]
 
-        gen_doc = []
-        line = ''
-        for list in gen_docs:
-            for i in list:
-                line += i + ' '
-            r = tokenize(line)
-            gen_doc.append(r)
-            line = ''
-
-        gen_docs = gen_doc
+        gen_docs = alphabet_tokenize(gen_docs)
         dictionary = gensim.corpora.Dictionary(gen_docs)
 
         corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
@@ -105,28 +90,15 @@ def student(request):
         sims = gensim.similarities.Similarity(
             ".\similarity_index\\similarity", tf_idf[corpus], num_features=len(dictionary))
 
-        filedocument2 = []
-        sentences = sent_tokenize(skills)
-        for sentence in sentences:
-            filedocument2.append(sentence)
+        filedocument2 = sentence_tokenize(skills)
 
         gen_docs2 = [[w.lower() for w in word_tokenize(text)]
                      for text in filedocument2]
 
         gen_docs2 = [[w for w in list if w not in sw] for list in gen_docs2]
 
-        gen_doc2 = []
-        line = ''
-        for list in gen_docs2:
-            for i in list:
-                line += i + ' '
-            r = tokenize(line)
-            gen_doc2.append(r)
-            line = ''
+        gen_docs2 = alphabet_tokenize(gen_docs2)
 
-        gen_docs2 = gen_doc2
-
-        # dictionary2 = gensim.corpora.Dictionary(gen_docs2)
         corpus2 = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs2]
 
         query_doc_tf_idf2 = tf_idf[corpus2]
@@ -167,9 +139,6 @@ def student(request):
             if Skills.objects.filter(skill_name=i.lower()).exists():
                 actual_absent_skills.append(i)
 
-        # print(actual_present_skills)
-        # print('------------------------')
-        # print(actual_absent_skills)
         context = percentage_of_similarity
         return redirect(check_compatibility, context=context)
 
@@ -191,7 +160,6 @@ def view_student(request):
     return render(request, 'home-templates/view_student.html', {'student_list': student_list})
 
 
-'''
 def view_compatibility(request):
     check_display_company = True
     check_set_company = False
@@ -210,7 +178,6 @@ def view_compatibility(request):
                            'check_display_company': check_display_company})
         if request.POST.get('get_role') == "submit_role":
             role_id = request.POST.get('selected_role')
-            selected_role = Jd.objects.filter(id=role_id).first()
             student_list = Student.objects.all()
             check_display_company = False
             check_select_students = True
@@ -222,17 +189,21 @@ def view_compatibility(request):
                            'check_display_company': check_display_company,
                            'check_select_students': check_select_students,
                            'check_set_company': check_set_company,
-                           'student_list': student_list})
+                           'student_list': student_list,
+                           'selected_role_id': role_id})
 
         if request.POST.get('get_students') == "submit_students":
             selected_students = request.POST.getlist('selected_students[]')
-            messages.success(request, f'{selected_students}')
-            return render(request, 'home-templates/view_compatibility.html',
-                          {
-                              'check_display_company': check_display_company,
-                              'check_select_students': check_select_students,
-                              'check_set_company': check_set_company,
-                          })
+            selected_role_id = request.POST.get('selected_role_id')
+
+            return redirect(stfu, selected_students=selected_students, selected_role_id=selected_role_id)
+            # messages.success(request, f'{selected_students}')
+            # return render(request, 'home-templates/view_compatibility.html',
+            #               {
+            #                   'check_display_company': check_display_company,
+            #                   'check_select_students': check_select_students,
+            #                   'check_set_company': check_set_company,
+            #               })
 
         return render(request, 'home-templates/view_compatibility.html',
                       {'companies_list': companies_list,
@@ -241,13 +212,6 @@ def view_compatibility(request):
                        'check_select_students': check_select_students})
     else:
         return render(request, 'home-templates/view_compatibility.html', {'companies_list': companies_list, 'check_display_company': check_display_company})
-        if request.POST.get('check_company') == 'Add Company':
-            print('HELLOOOOOOOOOOOOO!!!!!!!!!!!!!!!!')
-            messages.success(request, f'user clicked summary')
-            return render(request, 'home-templates/view_compatibility.html',{'companies_list':companies_list})
-    companies_list=Company.objects.all()
-    return render(request, 'home-templates/view_compatibility.html',{'companies_list':companies_list})
-'''
 
 
 def add_company(request):
@@ -301,7 +265,8 @@ def add_student(request):
         student.phone_number = request.POST.get('phone_number')
         student.email = request.POST.get('email')
         student.cgpa = request.POST.get('cgpa')
-        student.placement = request.POST.get('placement')
+        student.placement = 'Unplaced'
+        student.skills = request.POST.get('skills')
         student.save()
         messages.success(request, f'Student Added - {student.student_name}!')
         # CHANGE THE RENDER ADDRESS
@@ -319,7 +284,106 @@ def recruiter(request):
     return render(request, 'home-templates/recruiter.html')
 
 
+def stfu(request, selected_students, selected_role_id):
+    selected_role = Jd.objects.filter(id=selected_role_id).first()
+    jd = selected_role.get_job_desc()
+
+    num_list = []
+    skill_list = []
+    left_quote = False
+    right_quote = False
+    val = ''
+    for i in range(len(selected_students)):
+
+        if selected_students[i] == '\'':
+            if not left_quote:
+                left_quote = True
+                continue
+        if selected_students[i] != '\'':
+            if left_quote == True and right_quote == False:
+                val += selected_students[i]
+                if selected_students[i+1] == '\'':
+                    right_quote = True
+            elif left_quote == True and right_quote == True:
+
+                num_list.append(int(val))
+                val = ''
+                left_quote = False
+                right_quote = False
+                continue
+            else:
+                continue
+
+    for i in num_list:
+
+        skill = Student.objects.filter(pk=int(i)).first()
+        skill_list.append(skill.skills)
+
+    filedocument = sentence_tokenize(jd)
+
+    gen_docs = [[w.lower() for w in word_tokenize(text)]
+                for text in filedocument]
+    sw = set(stopwords.words('english'))
+    gen_docs = [[w for w in list if w not in sw] for list in gen_docs]
+
+    gen_docs = alphabet_tokenize(gen_docs)
+
+    dictionary = gensim.corpora.Dictionary(gen_docs)
+
+    corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
+    len_gen = len(gen_docs)
+    tf_idf = gensim.models.TfidfModel(corpus)
+    sims = gensim.similarities.Similarity(
+        ".\similarity_index\\similarity", tf_idf[corpus], num_features=len(dictionary))
+
+    similarity_scores = []
+    for i in skill_list:
+
+        filedocument2 = sentence_tokenize(i)
+
+        gen_docs2 = [[w.lower() for w in word_tokenize(text)]
+                     for text in filedocument2]
+
+        gen_docs2 = [[w for w in list if w not in sw] for list in gen_docs2]
+
+        gen_docs2 = alphabet_tokenize(gen_docs2)
+
+        # dictionary2 = gensim.corpora.Dictionary(gen_docs2)
+        corpus2 = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs2]
+
+        query_doc_tf_idf2 = tf_idf[corpus2]
+        sum_of_sims = (np.sum(sims[query_doc_tf_idf2], dtype=np.float32))
+        percentage_of_similarity = round(
+            float((sum_of_sims/len_gen) * 100))
+        if percentage_of_similarity > 100:
+            percentage_of_similarity = 100
+
+        similarity_scores.append(percentage_of_similarity)
+
+    return render(request, 'home-templates/stfu.html', {'similarity_scores': similarity_scores})
+
+
 def tokenize(text):
     # obtains tokens with a least 1 alphabet
     pattern = re.compile(r'[A-Za-z]+[\w^\']*|[\w^\']*[A-Za-z]+[\w^\']*')
     return pattern.findall(text.lower())
+
+
+def sentence_tokenize(text):
+    filedocument = []
+    sentences = sent_tokenize(text)
+    for sentence in sentences:
+        filedocument.append(sentence)
+    return filedocument
+
+
+def alphabet_tokenize(gen_docs):
+    gen_doc = []
+    line = ''
+    for list in gen_docs:
+        for i in list:
+            line += i + ' '
+        r = tokenize(line)
+        gen_doc.append(r)
+        line = ''
+    return gen_doc
