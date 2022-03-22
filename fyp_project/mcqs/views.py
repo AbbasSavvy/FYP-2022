76681e5593b61e78be7f6ee5e118235254337793
vaishnavi-ui <65065616+vaishnavi-ui@users.noreply.github.com>
2022-03-22 12:16:43 +0530

    mcq present skills
    use present and absent skills for mcqs. Display ques, ans in table and pass individual skill score to analytics page for pie chart

--- fyp_project/mcqs/views.py
+++ fyp_project/mcqs/views.py
@@ -30,7 +30,7 @@ def mcq_csv(request):
         form.save()
         form = McqModelForm()
         obj = Mcq_Skills.objects.get(activated=False)
-        with open(obj.mcq_file.path, 'r') as f:
+        with open(obj.mcq_file.path, 'r',encoding="UTF-8") as f:
             reader = csv.reader(f)
 
             for i, row in enumerate(reader):
@@ -60,15 +60,26 @@ def mcq_csv(request):
 
 def mcq_ques(request,present_skills,absent_skills):
     global qna_dict,skill_set,ques_bank,ans_bank,qna_skill,ques_skill_id,score_dict
-    #actual_present_skills=['python','java','css']
-    #actual_absent_skills=['c','sql']
-    print(present_skills)
+
+    present_skills=present_skills[1:]
+    present_skills=present_skills[:-1]
+    present_skills=present_skills.replace("'",'')
+    present_skills=present_skills.split(',')
     len_present = len(present_skills)
+
+    absent_skills=absent_skills[1:]
+    absent_skills=absent_skills[:-1]
+    absent_skills=absent_skills.replace("'",'')
+    absent_skills=absent_skills.split(',')
+    len_absent = len(absent_skills)
 
     if request.method == 'POST':
         if request.POST.get('skill_submit'):
             for i in range(len_present):
+                present_skills[i]=present_skills[i].lstrip()
                 id_skill = Skills.objects.filter(skill_name=present_skills[i])
+                print(id_skill)
+                print(present_skills[i])
                 if id_skill.exists:
                     for j in id_skill:
                         get_id = str(j)
@@ -95,18 +106,15 @@ def mcq_ques(request,present_skills,absent_skills):
                 for b in range(10):
                     qna_skill.append(skill_set[a][1])
 
-            print(qna_skill)
+            #print(qna_skill)
             qna_dict = dict(zip(ques_bank, ans_bank))
-            print(qna_dict)
+            #print(qna_dict)
             ques_skill_id=dict(zip(ques_bank,qna_skill))
-            print(ques_skill_id)
-            #qna_skill_dict=dict(zip(qna_skill,qna_dict))
-            #print(qna_skill_dict)
+            #print(ques_skill_id)
 
-            return render(request, 'mcqs-templates/mcq.html', {'present_skills': present_skills, 'absent_skills': absent_skills, 'qna_dict': qna_dict,'p':p})
+            return render(request, 'mcqs-templates/mcq.html', {'present_skills': present_skills, 'absent_skills': absent_skills, 'qna_dict': qna_dict})
 
         if request.POST.get('quiz_submit'):
-            #print("inside quiz submit")
             score=0
             count=1
             your_ans=[]
@@ -120,63 +128,15 @@ def mcq_ques(request,present_skills,absent_skills):
                 j='choice'+str(count)
                 option=request.POST.get(j)
                 your_ans.append(option)
-                #print(value)
-                #print(f'Answer:{value[2]}')
-                #print('----------------------------------------')
                 if(option==value[2]):
-                    #if(key==ques_skill_id[])
                     tp=ques_skill_id[key]
                     score_dict[tp]+=1
                     score+=1
                 count+=1
-            print(your_ans)
-            print(score_dict)
 
-            #your_ans_dict = dict(zip(your_ans, qna_dict))
-            # print(your_ans_dict)
-            return render(request, 'mcqs-templates/mcq_analytics.html', {'score': score, 'len_dict': len_dict, 'qna_dict': qna_dict, 'your_ans': your_ans})
+            return render(request, 'mcqs-templates/mcq_analytics.html', {'score': score, 'len_dict': len_dict, 'qna_dict': qna_dict, 'your_ans': your_ans, 'score_dict':score_dict})
 
         return render(request, 'mcqs-templates/mcq.html')
 
     else:
         return render(request, 'mcqs-templates/mcq.html')
-
-    '''
-    if request.method=='POST':
-        if request.POST.get('skill_submit'):
-            skill_key=request.POST.get('skill_name')
-            id_skill=Skills.objects.filter(skill_name=skill_key)
-
-            for i in id_skill:
-                get_id=i
-            id_of_skill=get_id
-
-
-            skill_ques=Ques_Ans.objects.filter(skill_id=get_id).values('ques')
-            return render(request, 'mcqs-templates/mcq.html',{'skills':id_skill,'mcqs':skill_ques})
-
-        if request.POST.get('quiz_submit'):
-            get_id=id_of_skill
-            get_id=2
-            print(get_id)
-            score=0
-            ideal_ans=Ques_Ans.objects.filter(skill_id=get_id).values('ans')
-            count=1
-            for i in ideal_ans:
-                for key, value in i.items():
-                    j='choice'+str(count)
-                    option=request.POST.get(j)
-                    print(value)
-                    print(f'Answer:{value[2]}')
-                    print('----------------------------------------')
-                    if(option==value[2]):
-                        score+=1
-                    count+=1
-
-
-
-            return render(request, 'mcqs-templates/mcq.html',{'score':score})
-
-    else:
-        return render(request, 'mcqs-templates/mcq.html')
-'''
